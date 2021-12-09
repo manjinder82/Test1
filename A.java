6ba788e566017b56f1938d4cf1d453158cd17bff
Manjinder Singh <manjinder.singh@techmahindra.com>
2021-12-09 14:28:19 +0530

    1st Version

---
+++ A.java
@@ -0,0 +1,27 @@
+class Animal {
+	void eat()
+	{
+		System.out.println("Eating");
+	}
+}
+class Dog extends Animal{
+	void bark()
+	{
+		System.out.println("Barking");
+	}
+}
+class Cat extends Animal{
+	void meow()
+	{
+		System.out.println("Meowing");
+}
+}
+class A
+{
+	public static void main(String args[])
+	{
+		Cat c = new Cat();
+		c.meow();
+		
+	}
+}
